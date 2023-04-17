ee6e4a927532f4189402296a4d1fe42b7cb4aa50
entegregory <entegregory@gmail.com>
2023-04-18 04:24:53 +0600

    feat: macaddress

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,10 +14,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(inputString) {
+  // Создаем регулярное выражение для проверки MAC-48 адреса
+  // \b - указывает на границу слова
+  // [0-9A-F] - ожидаем символы от 0 до 9 или от A до F (шестнадцатеричные цифры)
+  // [0-9A-F]{2} - ожидаем две шестнадцатеричные цифры
+  // (?:-[0-9A-F]{2}){5} - ожидаем 5 групп, состоящих из дефиса и двух шестнадцатеричных цифр
+  // \b - указывает на границу слова
+  const regex = /\b[0-9A-F]{2}(?:-[0-9A-F]{2}){5}\b/;
+
+  // Выполняем проверку строки inputString с помощью регулярного выражения
+  // Если строка соответствует паттерну, то функция вернет true, иначе false
+  return regex.test(inputString);
 }
+
 module.exports = {
   isMAC48Address
 };
