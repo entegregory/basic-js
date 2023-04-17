9d8483b24d2ee28220f8bd42ff6e6f87edc38813
entegregory <entegregory@gmail.com>
2023-04-18 05:28:28 +0600

    feat: add repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  // Устанавливаем значения по умолчанию для параметров
+  options.repeatTimes = options.repeatTimes || 1;
+  options.separator = options.separator || '+';
+  options.addition = options.hasOwnProperty('addition') ? String(options.addition) : '';
+  options.additionRepeatTimes = options.additionRepeatTimes || 1;
+  options.additionSeparator = options.additionSeparator || '|';
+
+  // Создаем строку с добавлениями, повторяем additionRepeatTimes раз с разделителем additionSeparator
+  const additionString = new Array(options.additionRepeatTimes).fill(options.addition).join(options.additionSeparator);
+
+  // Создаем итоговую строку, повторяем str и добавляем additionString repeatTimes раз с разделителем separator
+  return new Array(options.repeatTimes).fill(str + additionString).join(options.separator);
 }
 
 module.exports = {
