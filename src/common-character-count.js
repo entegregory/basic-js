15b664f1db7aaf29d531c4aac4e3c3f97be87998
entegregory <entegregory@gmail.com>
2023-04-18 05:57:29 +0600

    feat: add some

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let count = 0;
+  s1 = s1.split('');
+  s2 = s2.split('');
+
+  // Проходим по каждому символу первой строки
+  for (let i = 0; i < s1.length; i++) {
+    // Находим индекс символа во второй строке
+    const index = s2.indexOf(s1[i]);
+
+    // Если символ найден, удаляем его из второй строки и увеличиваем счетчик
+    if (index !== -1) {
+      s2.splice(index, 1);
+      count++;
+    }
+  }
+
+  return count;
 }
 
 module.exports = {
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  // Преобразуем число в строку и затем в массив символов
+  const digits = String(n).split('');
+  let maxNumber = 0;
+
+  // Проходим по массиву цифр
+  for (let i = 0; i < digits.length; i++) {
+    // Создаем новый массив без текущего элемента
+    const newDigits = digits.slice(0, i).concat(digits.slice(i + 1));
+
+    // Преобразуем новый массив обратно в число
+    const newNumber = parseInt(newDigits.join(''), 10);
+
+    // Если полученное число больше текущего максимального, обновляем значение maxNumber
+    if (newNumber > maxNumber) {
+      maxNumber = newNumber;
+    }
+  }
+
+  return maxNumber;
 }
 
 module.exports = {
