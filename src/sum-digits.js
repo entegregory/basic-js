d3ddc7dbff3f9fa15a36ab79e7a43d0a35025c4f
entegregory <entegregory@gmail.com>
2023-04-18 04:50:51 +0600

    feat: sum

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,20 +1,29 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
 /**
- * Given a number, replace this number with
- * the sum of its digits until we get to a one digit number.
+ * Дано число, замените это число
+ * суммой его цифр, пока мы не получим однозначное число.
  *
  * @param {Number} n
  * @return {Number}
  *
  * @example
- * For 100, the result should be 1 (1 + 0 + 0 = 1)
- * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
+ * Для 100 результат должен быть 1 (1 + 0 + 0 = 1)
+ * Для 91 результат должен быть 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  // Преобразование числа в строку для получения массива цифр
+  let digits = n.toString().split('');
+
+  // Пока массив содержит более одного элемента, продолжаем суммировать цифры
+  while (digits.length > 1) {
+    // Суммируем все цифры числа и преобразуем результат обратно в массив цифр
+    digits = digits.reduce((sum, digit) => sum + parseInt(digit), 0).toString().split('');
+  }
+
+  // Возвращаем однозначное число
+  return parseInt(digits[0]);
 }
 
 module.exports = {
