696f05cb65e492bc9bd1b3391d81ed45a70e98f7
entegregory <entegregory@gmail.com>
2023-04-18 05:17:51 +0600

    feat: add chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [], // массив для хранения звеньев цепи
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length; // возвращает текущую длину цепи в виде числа
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    this.chain.push(`( ${value === undefined ? '' : value} )`); // добавляет звено, содержащее строковое представление значения, в цепь
+    return this; // делаем метод addLink цепочечным
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    if (typeof position !== 'number' || position <= 0 || position > this.chain.length || !Number.isInteger(position)) {
+      this.chain = []; // очищаем текущую цепь
+      throw new Error("You can't remove incorrect link!"); // выбрасываем ошибку, если позиция недопустима
+    }
+    this.chain.splice(position - 1, 1); // удаляем звено цепи на указанной позиции
+    return this; // делаем метод removeLink цепочечным
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse(); // разворачиваем цепь
+    return this; // делаем метод reverseChain цепочечным
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const finishedChain = this.chain.join('~~'); // завершаем цепь и преобразуем ее в строку
+    this.chain = []; // очищаем текущую цепь
+    return finishedChain; // возвращаем завершенную цепь
   }
 };
 
