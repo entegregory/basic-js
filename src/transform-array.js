f33f4bf5dc3d33cbd9adc6d88c622432375d9ba8
entegregory <entegregory@gmail.com>
2023-04-18 05:11:46 +0600

    feat: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  // Проверяем, является ли arr экземпляром массива. Если нет, выбрасываем ошибку.
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  // Создаем новый массив для хранения преобразованных элементов
+  let transformed = [];
+
+  // Обходим элементы массива arr
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-next':
+        // Исключаем следующий элемент из массива, если он существует
+        if (i < arr.length - 1) {
+          i++;
+        }
+        break;
+      case '--discard-prev':
+        // Исключаем предыдущий элемент из массива, если он существует и не был удален
+        if (i > 0 && arr[i - 1] !== '--deleted') {
+          transformed.pop();
+        }
+        break;
+      case '--double-next':
+        // Дублируем следующий элемент массива, если он существует
+        if (i < arr.length - 1) {
+          transformed.push(arr[i + 1]);
+        }
+        break;
+      case '--double-prev':
+        // Дублируем предыдущий элемент массива, если он существует и не был удален
+        if (i > 0 && arr[i - 1] !== '--deleted') {
+          transformed.push(arr[i - 1]);
+        }
+        break;
+      default:
+        // Добавляем текущий элемент в преобразованный массив
+        transformed.push(arr[i]);
+    }
+  }
+
+  // Возвращаем преобразованный массив
+  return transformed;
 }
 
 module.exports = {
